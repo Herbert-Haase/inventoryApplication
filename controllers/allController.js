c4299894a9a13340f0d69c40f4eafa6f94b700fc
Herbert-Haase <herberthaase02@gmx.de>
2024-07-28 15:54:47 +0200

    feat: create template for homepage

--- controllers/allController.js
+++ controllers/allController.js
@@ -5,12 +5,14 @@ exports.pokemonsTrainersTypesGet = asyncHandler(async (req, res) => {
   const trainers = await db.getAllTrainers();
   const pokemons = await db.getAllPokemons();
   const types = await db.getAllTypes();
-  res.send(
-    "Trainer: " +
-      trainers.map((trainer) => trainer.name).join(", ") +
-      "\nPokemon: " +
-      pokemons.map((pokemon) => pokemon.name).join(", ") +
-      "\nType: " +
-      types.map((type) => type.type).join(", ")
-  );
+
+  res.render("all", { trainers: trainers, pokemons: pokemons, types: types });
+  //   res.send(
+  //     "Trainer: " +
+  //       trainers.map((trainer) => trainer.name).join(", ") +
+  //       "\nPokemon: " +
+  //       pokemons.map((pokemon) => pokemon.name).join(", ") +
+  //       "\nType: " +
+  //       types.map((type) => type.type).join(", ")
+  //   );
 });
